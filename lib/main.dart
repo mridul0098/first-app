b54f3d9effc2602231ed36df8216ce8d607029ef
mridul rahman <92113512+mridul0098@users.noreply.github.com>
2021-12-14 09:05:19 +0600

    first app

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main(){
+   runApp(
+     MaterialApp(
+       title: 'mridul',
+       home: Scaffold(
+         body:Center(
+          child: Text("hello world") 
+         ),
+       ),
+      )
+   );
+}
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
